881b60bac2da0e70786ab6861187d33986e3c092
Marquis Taylor <marquistaylor@gmail.com>
2020-06-11 16:11:46 -0400

    June 11

---
+++ Mark_Window.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Rocket Base Station")
+
+
+x= 50
+y= 50
+width= 40
+height = 60
+
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    pygame.draw.rect(win, (255,0, 0),(x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
+print("Program Closed")
