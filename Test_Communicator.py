17d3c54063dc806e2bf378294fdf18fe500ef0ae
Marquis Taylor <marquistaylor@gmail.com>
2020-07-03 13:36:01 -0400

    Added Communicator for Receiving Actual Data

--- Test_Communicator.py
+++ Test_Communicator.py
@@ -13,6 +13,7 @@ class Communicator():
     def refresh(self):
 
         if random.random() > 0.9:
+
             self.rocket.temps.append(round(random.uniform(10, 30), 2))
             self.rocket.air_pressure.append(round(random.uniform(1000, 1100),2))
             self.rocket._is_changed = True
