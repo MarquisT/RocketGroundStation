09591eb67a9c3822e816643651d81b497b1ab5aa
Marquis Taylor <marquistaylor@gmail.com>
2020-07-03 12:39:40 -0400

    Added Pressure to the Rocket

--- Test_Communicator.py
+++ Test_Communicator.py
@@ -14,4 +14,5 @@ class Communicator():
 
         if random.random() > 0.9:
             self.rocket.temps.append(round(random.uniform(10, 30), 2))
+            self.rocket.air_pressure.append(round(random.uniform(1000, 1100),2))
             self.rocket._is_changed = True
